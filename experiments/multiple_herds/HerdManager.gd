139a0a00140a18098d032d8f0f7f575cea2ed72f
kiesen <creative77@gmail.com>
2020-04-11 20:20:39 +1000

    herdling UI

--- experiments/multiple_herds/HerdManager.gd
+++ experiments/multiple_herds/HerdManager.gd
@@ -7,8 +7,28 @@ var target : Vector2 = Vector2() setget set_target
 var leader = null
 
 func _ready():
-	pass
+	if get_child_count():
+		for c in get_children():
+			add_herdling(c)
 
+# herd leader logic
+func pick_leader(l : KinematicBody2D = null) -> void:
+	if l:
+		leader.is_leader = false
+		leader = l
+	else:
+		if leader: 
+			leader.is_leader = false
+			leader = null
+		
+		if len(herd):
+			leader = herd[randi() % len(herd)]
+			leader.is_leader = true
+
+func validate_leader() -> bool:
+	return false
+
+# herd navigation logic
 func set_target(t : Vector2) -> void:
 	target = t
 
@@ -26,6 +46,15 @@ func add_herdling(herdling : Node) -> void:
 	herdling.herd = self
 
 func remove_herdling(herdling : Node) -> void:
-	herd.erase(herdling)
-	remove_child(herdling)
+	if herdling in herd:
+		herd.erase(herdling)
+	
+	if herdling.get_parent() == self:
+		remove_child(herdling)
+	
+	if herdling == leader:
+		herdling.is_leader = false
+		leader = null
+		pick_leader()
+	
 	herdling.herd = null
--- experiments/multiple_herds/herdling/Herdling.gd
+++ experiments/multiple_herds/herdling/Herdling.gd
@@ -1,12 +1,53 @@
 extends KinematicBody2D
 
-# the intention is to use a state machine
+var herd : Node
+var is_leader : bool = false
+
+var mouse_hover : bool = false
+var info_lock : bool = false
 
 func _ready():
 	pass
 
+
+# the intention is to use a state machine
 func _process(delta):
 	$StateMachine.process(delta)
+	
+	if mouse_hover or info_lock:
+		update_info()
 
 func _physics_process(delta):
 	$StateMachine.physics_process(delta)
+
+#info stuffs
+func _unhandled_input(event):
+	if mouse_hover and event.is_action_released("secondary_click"):
+		info_lock = !info_lock
+
+func update_info() -> void:
+	var r = $RichTextLabel
+	
+	r.clear()
+	r.push_align(RichTextLabel.ALIGN_CENTER)
+	
+	r.push_bold()
+	r.add_text("State: ")
+	r.pop()
+	r.add_text($StateMachine.state.name)
+	
+	if is_leader:
+		r.newline()
+		r.push_bold()
+		r.add_text("Is Leader")
+		r.pop()
+	
+	r.pop()
+
+func _on_mouse_entered():
+	mouse_hover = true
+	$RichTextLabel.show()
+
+func _on_mouse_exited():
+	mouse_hover = false
+	$RichTextLabel.visible = info_lock
